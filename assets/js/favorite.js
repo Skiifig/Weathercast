d8ba68cc186337e72a01677043933a8d9b2a0497
Tristan <tristan.reig@gmail.com>
2023-05-03 19:51:17 +0200

    Désélection des favoris depuis l'index + alertes

--- assets/js/favorite.js
+++ assets/js/favorite.js
@@ -1,13 +1,17 @@
 var tabFav = JSON.parse(localStorage.getItem("favoris")) || [];
 
-function showAlert(id, type) {
+function showAlert(type, status_code=0, id=null) {
   var alert = document.getElementById('alert-' + type);
-  var text = document.getElementById('alert-text')
+  var text = document.getElementById('alert-text');
   alert.classList.replace('opacity-0', 'opacity-1');
-  if (type == 'danger') {
-    text.innerHTML = "Cette ville n'a pas pu être ajouter à votre liste de favoris."
-  } else {
-    text.innerHTML = id + " a bien été ajouté à votre liste de favoris."
+  if (type == 'warning') {
+    text.innerHTML = "L'historique est vide."
+  } else if (type == 'success') {
+      if (status_code == 1) {
+        text.innerHTML = id + " a bien été ajouté à votre liste de favoris."
+      } else if (status_code == -1) {
+        text.innerHTML = id + " a bien été retiré de votre liste de favoris."
+      }
   }
   setTimeout(() => {
     alert.classList.replace('opacity-1', 'opacity-0');
@@ -29,19 +33,24 @@ function cleanFav() {
 }
 
 function changeFav(event) {
+  var tabFav = JSON.parse(localStorage.getItem("favoris")) || [];
   var id = event.target.dataset.id;
-  if (id == undefined) {
-    showAlert(id, 'danger')
+  if (tabFav.includes(id)) {
+    event.target.setAttribute('fill', 'none')
+    index = tabFav.indexOf(id)
+    tabFav.splice(index, 1)
+    localStorage.setItem("favoris", JSON.stringify(tabFav))
+    showAlert('success', -1, id)
   }
   else if (!tabFav.includes(id)) {
     event.target.setAttribute('fill', 'red');
     tabFav.push(id)
     localStorage.setItem("favoris", JSON.stringify(tabFav))
-    showAlert(id, 'success')
+    showAlert('success', 1, id)
   }
 }
 
-function removeFav() {
+function removeFavF() {
   const checkedBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
   for (const checkedBox of checkedBoxes) {
     index = checkedBox.id.split('-')[1];
--- main.js
+++ main.js
@@ -15,5 +15,5 @@ app.on("ready", () => {
     },
   });
   mainWindow.webContents.openDevTools()
-  mainWindow.loadURL(`file://${__dirname}/about.html`);
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
